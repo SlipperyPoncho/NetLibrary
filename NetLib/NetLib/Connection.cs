bf38646be1543a53ff41df2c710ae18e2c4b89ca
human3562 <human3562@gmail.com>
2023-03-15 15:39:26 +0300

    minor cleanup

--- NetLib/NetLib/Connection.cs
+++ NetLib/NetLib/Connection.cs
@@ -7,7 +7,7 @@ using System.Text;
 namespace NetLib
 {
 
-    public class NetMessage {
+    public struct NetMessage {
 
         //public Packet packet { get; set; }
         public string message { get; set; }
@@ -16,57 +16,59 @@ namespace NetLib
 
     public class Connection
     {
+        public ConcurrentQueue<NetMessage> q_incomingMessages = new();
+        
         private Thread _networkListener;
-
         private UdpClient client;  
-
-        private ConcurrentQueue<NetMessage> q_incomingMessages = new();
-
         private bool _connectionRunning = false;
-
         private int port;
 
-       public Connection(int port) {
-            client = new(port);
-            this.port = port;
-       }
-       
-       public void Start() {
-           _connectionRunning = true;
-           _networkListener = new Thread(new ThreadStart(_networkRun));
-           _networkListener.Start();
-       }
-       
-       public void Close() {
-           _connectionRunning = false;
-       }
-       
-       public void Send(IPEndPoint receiver, string msg) {
-           byte[] sendbuff = Encoding.ASCII.GetBytes(msg);
-           client.Send(sendbuff, receiver);
-           Console.WriteLine("Sending message " + sendbuff.Length);
-       }
-       
-       //separate thread
-       private void _networkRun() {
-           if (!_connectionRunning) return;
-           IPEndPoint connection = new(IPAddress.Any, port);
-           Console.WriteLine("Connection network listener started...");
-       
-           while (_connectionRunning) {
-               bool canRead = client.Available > 0;
-               if (canRead) {
-                   byte[] data = client.Receive(ref connection);
-                   Console.WriteLine($"Connection received broadcast from {connection}");
+        public Connection(int port) {
+             this.port = port;
+             client = new(port);
+        
+             _networkListener = new Thread(new ThreadStart(_networkReceive));
+        }
+        
+        public void Start() {
+            _connectionRunning = true;
+            _networkListener.Start();
+        }
+        
+        public void Close() {
+            _connectionRunning = false;
+        }
+        
+        public void Send(IPEndPoint receiver, string msg) {
+            byte[] sendbuff = Encoding.ASCII.GetBytes(msg);
+            client.Send(sendbuff, receiver);                  
+            Console.WriteLine("Sending message " + sendbuff.Length);
+        }
+
+        //separate thread
+        private void _networkReceive() {
+            if (!_connectionRunning) return;
+
+            IPEndPoint connection = new(IPAddress.Any, port);
+            Console.WriteLine("Connection network listener started...");
+        
+            while (_connectionRunning) {
+                bool canRead = client.Available > 0;
+                if (canRead) {
+                    byte[] data = client.Receive(ref connection);
+                    Console.WriteLine($"Connection received broadcast from {connection}");
                     Console.WriteLine($"Message is as follows: {Encoding.ASCII.GetString(data, 0, data.Length)}");
-                   q_incomingMessages.Enqueue(new NetMessage {
-                       message = Encoding.ASCII.GetString(data, 0, data.Length),
-                       RecieveTime = DateTime.Now
-                   });
-               }
-           }
-       
-       }
+                    q_incomingMessages.Enqueue(new NetMessage {
+                        message = Encoding.ASCII.GetString(data, 0, data.Length),
+                        RecieveTime = DateTime.Now
+                    });
+                }
+                else {
+                    Thread.Sleep(1);
+                }
+            }
+        
+        }
 
     }
 }
